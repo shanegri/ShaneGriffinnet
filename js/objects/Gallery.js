f91d0d8e0e1795853ed439d6f878f5b2373beebe
Shane <shgriffin16@gmail.com>
2017-11-24 17:34:15 -0500

    init gallery loading

--- js/objects/Gallery.js
+++ js/objects/Gallery.js
@@ -1,7 +1,33 @@
 function Gallery(){
 
 
+  this.images = new Array();
+  this.getImages = () => {
+    let images = this.images;
+    let url = window.location.protocol + "//"
+              + window.location.hostname + "/Images/Gallery/";
+    $.ajax({
+      url: url,
+      success: (data) => {
+        $(data).find("td > a").each(function(i){
+          if(i != 0){
+            ($(this).attr("href"));
+          }
+        })
+      }
+    });
+  }
 
+
+  this.init = () => {
+
+  }
+
+  this.getImages();
+  console.log(this.images);
+}
+
+function Image(){
 
 
 }
